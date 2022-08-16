18096a78d8d9cf3e8961884f2c92b48bb1387465
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-08-16 10:17:19 +0700

    created the project

---
+++ src/main/java/com/qwerty/pastebook/PasteBookApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty.pastebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qwerty/pastebook/PasteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerty.pastebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
