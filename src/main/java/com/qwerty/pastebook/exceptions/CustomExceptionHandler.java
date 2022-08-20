96f5282202cbf383e625d06cba12536dae4a1278
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-08-20 22:48:36 +0700

    added new exception handling

--- src/main/java/com/qwerty/pastebook/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/qwerty/pastebook/exceptions/CustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.qwerty.pastebook.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.NotReadablePropertyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -31,6 +33,27 @@ public class CustomExceptionHandler {
             });
         }
         return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(BadRequestException e) {
+        Map<String, String> message = new HashMap<>();
+        message.put("message", e.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotReadablePropertyException.class)
+    public ResponseEntity<?> handleNotReadableExceptions(NotReadablePropertyException exception) {
+        Map<String, String> message = new HashMap<>();
+        message.put("message", "some data are strange");
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException e) {
+        Map<String, String> message = new HashMap<>();
+        message.put("message", "username and/or password invalid");
+        return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = {Exception.class})
