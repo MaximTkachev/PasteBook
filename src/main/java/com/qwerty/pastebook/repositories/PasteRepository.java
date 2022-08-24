48e4a1389456cd91c63db3a1209cc469fbb7ba87
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-08-24 21:22:21 +0700

    added methods for paste requests
    added a method for getting the last 10 pastes and a method for getting user pastes

--- src/main/java/com/qwerty/pastebook/repositories/PasteRepository.java
+++ src/main/java/com/qwerty/pastebook/repositories/PasteRepository.java
@@ -1,9 +1,22 @@
 package com.qwerty.pastebook.repositories;
 
 import com.qwerty.pastebook.entities.PasteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.qwerty.pastebook.entities.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
-public interface PasteRepository extends JpaRepository<PasteEntity, String> {
+public interface PasteRepository extends PagingAndSortingRepository<PasteEntity, String> {
+
+    @Query(value = "SELECT * FROM pastes WHERE access_modifier = 'PUBLIC' order by date_of_creation desc LIMIT 10",
+    nativeQuery = true)
+    List<PasteEntity> getTenLatest();
+
+    Page<PasteEntity> findAllByOwnerAndExpirationAfterOrExpirationNull(UserEntity owner, Date expiration, Pageable pageable);
 }
