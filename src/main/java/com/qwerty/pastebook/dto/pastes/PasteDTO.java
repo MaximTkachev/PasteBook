975c5b32295abbd0480c7152e808d02161f7dcea
MaximTkachev <maxim_tkachev_2002@mail.ru>
2022-08-31 15:50:14 +0700

    changed the format of the returned date

--- src/main/java/com/qwerty/pastebook/dto/pastes/PasteDTO.java
+++ src/main/java/com/qwerty/pastebook/dto/pastes/PasteDTO.java
@@ -1,10 +1,12 @@
 package com.qwerty.pastebook.dto.pastes;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.qwerty.pastebook.entities.AccessModifier;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -15,6 +17,7 @@ public class PasteDTO {
     private Long ownerId;
     private String title;
     private String text;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date expiration;
     private AccessModifier accessModifier;
 }
